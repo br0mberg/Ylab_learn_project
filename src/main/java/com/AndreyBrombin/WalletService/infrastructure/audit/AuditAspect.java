8813dd35e127bf9976999a493a54e5ca6215a929
Andrey <arcadka@gmail.com>
2023-11-06 15:50:58 +0300

    del_folder_tracking

--- src/main/java/com/AndreyBrombin/WalletService/infrastructure/audit/AuditAspect.java
+++ src/main/java/com/AndreyBrombin/WalletService/infrastructure/audit/AuditAspect.java
@@ -1,12 +1,7 @@
 package com.AndreyBrombin.WalletService.infrastructure.audit;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.JoinPoint;
 import java.util.Date;
 
-@Aspect
 public class AuditAspect {
 
 }
--- src/main/java/com/AndreyBrombin/WalletService/servlets/LoginServlet.java
+++
@@ -1,9 +0,0 @@
-package com.AndreyBrombin.WalletService.servlets;
-
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-
-@WebServlet("/login")
-public class LoginServlet extends HttpServlet {
-    
-}
